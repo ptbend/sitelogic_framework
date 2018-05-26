9a5987cb1c267166dcf06b8cc8d8456e8b9b8730
Peter <petertbender@gmail.com>
2018-05-26 07:55:46 -0500

    Initial Commit
    First Commit of WIP Site

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).foundation();
+
+// SVG / PNG
+  if(!Modernizr.svg) {
+    $('img[src*="svg"]').attr('src', function () {
+    return $(this).attr('src').replace('.svg', '.png');
+  });
+}
---
+++ js/min/app-min.js
@@ -0,0 +1,2 @@
+"use strict";$(document).foundation(),Modernizr.svg||$('img[src*="svg"]').attr("src",function(){return $(this).attr("src").replace(".svg",".png")});
+//# sourceMappingURL=./app-min.js.map
